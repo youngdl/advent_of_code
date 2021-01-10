b8260a965ecf7104db9efbc25813f2a6538cacbf
Xiaolin Yang <yangxiaolin.sbu@gmail.com>
2021-01-09 20:06:45 -0800

    Clean up day12_part2 to avoid run regex multiple times, and save calculations.

--- 2020/day12_part2.py
+++ 2020/day12_part2.py
@@ -7,54 +7,51 @@ def main():
 
     ship_pos = [0, 0]  #East-North is positive
     waypoint = [10, 1] #Relative to the ship, and east-north is positive
+    pattern = re.compile('^([FWENSLR])(\d+)')
+    
     for item in input_list:
-        m = re.match('^F(\d+)', item)
-        w = re.match('^W(\d+)', item)
-        e = re.match('^E(\d+)', item)
-        n = re.match('^N(\d+)', item)
-        s = re.match('^S(\d+)', item)
-        d = re.match('^([LR])(\d+)', item)
-
-        if w:
-            waypoint[0] = waypoint[0] - int(w.group(1))
-        if e:
-            waypoint[0] = waypoint[0] + int(e.group(1))
-        if n:
-            waypoint[1] = waypoint[1] + int(n.group(1))
-        if s:
-            waypoint[1] = waypoint[1] - int(s.group(1))
-        if m:
-            ship_pos[0] = ship_pos[0] + int(m.group(1)) * waypoint[0]
-            ship_pos[1] = ship_pos[1] + int(m.group(1)) * waypoint[1]
-        if d:
-            if d.group(1) == 'L':
-                if (int(d.group(2)) // 90) % 4 == 1:
-                    temp = waypoint[0]
-                    waypoint[0] = -1 * waypoint[1]
-                    waypoint[1] = temp
-                elif (int(d.group(2)) // 90) % 4 == 2:
-                    waypoint[0] = -1 * waypoint[0]
-                    waypoint[1] = -1 * waypoint[1]
-                elif (int(d.group(2))// 90) % 4 == 3:
-                    temp = waypoint[0]
-                    waypoint[0] = waypoint[1]
-                    waypoint[1] = -1 * temp
-                else:
-                    pass
+        match = pattern.match(item)
+        action = match.group(1)
+        value = match.group(2)
+        if action == 'W':
+            waypoint[0] = waypoint[0] - int(value)
+        if action == 'E':
+            waypoint[0] = waypoint[0] + int(value)
+        if action == 'N':
+            waypoint[1] = waypoint[1] + int(value)
+        if action == 'S':
+            waypoint[1] = waypoint[1] - int(value)
+        if action == 'F':
+            ship_pos[0] = ship_pos[0] + int(value) * waypoint[0]
+            ship_pos[1] = ship_pos[1] + int(value) * waypoint[1]
+        if action == 'L':
+            if (int(value) // 90) % 4 == 1:
+                temp = waypoint[0]
+                waypoint[0] = -1 * waypoint[1]
+                waypoint[1] = temp
+            elif (int(value) // 90) % 4 == 2:
+                waypoint[0] = -1 * waypoint[0]
+                waypoint[1] = -1 * waypoint[1]
+            elif (int(value)// 90) % 4 == 3:
+                temp = waypoint[0]
+                waypoint[0] = waypoint[1]
+                waypoint[1] = -1 * temp
             else:
-                if (int(d.group(2)) // 90) % 4 == 3:
-                    temp = waypoint[0]
-                    waypoint[0] = -1 * waypoint[1]
-                    waypoint[1] = temp
-                elif (int(d.group(2)) // 90) % 4 == 2:
-                    waypoint[0] = -1 * waypoint[0]
-                    waypoint[1] = -1 * waypoint[1]
-                elif (int(d.group(2)) // 90) % 4 == 1:
-                    temp = waypoint[0]
-                    waypoint[0] = waypoint[1]
-                    waypoint[1] = -1 * temp
-                else:
-                    pass
+                pass
+        if action == 'R':
+            if (int(value) // 90) % 4 == 3:
+                temp = waypoint[0]
+                waypoint[0] = -1 * waypoint[1]
+                waypoint[1] = temp
+            elif (int(value) // 90) % 4 == 2:
+                waypoint[0] = -1 * waypoint[0]
+                waypoint[1] = -1 * waypoint[1]
+            elif (int(value) // 90) % 4 == 1:
+                temp = waypoint[0]
+                waypoint[0] = waypoint[1]
+                waypoint[1] = -1 * temp
+            else:
+                pass
 
     print('east number is {}, north number is {}, and the Manhattan Distance is {}'.format(ship_pos[0], ship_pos[1], (abs(ship_pos[0]) + abs(ship_pos[1]))))
 
